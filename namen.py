8a0088af712c636146cca4d8cb25347d76b07efa
Thom Versigny <thomversigny@gmail.com>
2021-11-02 11:17:28 +0100

    code verbeterd

--- namen.py
+++ namen.py
@@ -2,11 +2,13 @@ def namenvragen(naam):
 	naam = input("Type jouw naam of stop om te stoppen: ").lower()
 	if naam == 'stop':
 		return naam
-	jaar = input("Type jouw leeftijd: ")
+	jaar = input("Type jouw leeftijd of stop om te stoppen: ")
+	if jaar == 'stop':
+		return jaar
 	print('hallo '+str(naam)+' en jouw leeftijd is '+str(jaar))
 
 while True:
+	namenvragen('naam')
 	if namenvragen('naam') == 'stop':
 		input('press enter to exit')
 		break
-	namenvragen('naam')
